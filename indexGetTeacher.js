0324caa380b75e7925789dd946a0f8ace23ffa8f
panguin06 <nate.h@skybridgeatx.com>
2022-11-03 10:38:02 -0500

    Create indexGetTeacher.js

---
+++ indexGetTeacher.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "testdb",
+  port: "3306"
+});
+
+con.query("SELECT * FROM teacher_list", function (err, rows) {
+  for (var i = 0; i < rows.length; i++) {
+    var row = rows[i];
+    console.log("<li value=\""+ i++ +"\"> <img src=\"/images/WaterDroplet.png\" alt=\"imageofAccountIcon\"> <button>" + row.COL1 + "</button></li>")
+}
+process.exit(0);
+});
