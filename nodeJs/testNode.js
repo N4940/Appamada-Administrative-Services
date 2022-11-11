bf0f7d58de5b38cb857caf62dd032fc2819a0e41
Napa-Appamada <71147375+N4940@users.noreply.github.com>
2022-11-10 22:56:10 -0600

    Added Teacher Selection precursor
    The beginner teacher select stuff without any of the SQL

--- nodeJs/testNode.js
+++ nodeJs/testNode.js
@@ -1,17 +1,17 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
+  host: "10.13.37.44",
+  user: "user",
+  password: "password",
   database: "testdb",
   port: "3306"
 });
 
-con.query("SELECT pStatus FROM table1 WHERE pName = 'Nate'", function (err, rows) {
+con.query("SELECT * FROM table1", function (err, rows) {
   for (var i = 0; i < rows.length; i++) {
     var row = rows[i];
-    console.log(row.pStatus);
-    process.exit(0);
+    console.log(row.pName);
 }
+process.exit(0);
 });
--- scripts/indexGetTeacher.js
+++ scripts/indexGetTeacher.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
+  host: "10.13.37.44",
+  user: "user",
+  password: "password",
   database: "testdb",
   port: "3306"
 });
@@ -11,7 +11,9 @@ var con = mysql.createConnection({
 con.query("SELECT * FROM teacher_list", function (err, rows) {
   for (var i = 0; i < rows.length; i++) {
     var row = rows[i];
+
     console.log("<li value=\""+ i +"\"> <img src=\"/images/WaterDroplet.png\" alt=\"imageofAccountIcon\"> <button>" + row.COL1 + "</button></li>")
+
 }
 process.exit(0);
 });
